9913b5c5cfb2ea6e35a07e278177080a733d0302
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2022-07-12 15:49:31 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificcationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-  <h1>Ola Mundo</h1>
+  return (
+    <>
+      <h1>Ola Mundo</h1>
+      <NotificcationButton />
+      <NotificcationButton />
+    </>
   )
 }
 
